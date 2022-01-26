437619acfcfe048d183a6297032cf29a0c8585b5
GiSeok-Hong <hgsok3056@naver.com>
2022-01-26 12:27:33 +0900

    자식컴포넌트 ProductAlertsComponent로 데이터 전달하기

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts', // 이 컴포넌트가 어떤 태그 이름으로 사용될지 결정
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  // ProductAlertsComponent 클래스 선언에 product 라는 프로퍼티를 선언하고 이 프로퍼티 앞에 @Input() 데코레이터를 지정한다.
+  // 프로퍼티에 @Input() 데코레이터를 지정하면 해당 프로퍼티의 값을 부모 컴포넌트에서 받아온다는 것을 의미한다. 여기에서는 ProductListComponent가 부모 컴포넌트이다.
+  @Input() productHello!: Product;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/products.ts
+++ src/app/products.ts
@@ -27,12 +27,7 @@ export const products = [
   {
     id: 4,
     name: 'Hong Phone',
+    price: 999,
     description: 'Hong is happy',
   },
 ];
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
